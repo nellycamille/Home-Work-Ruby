b402f8f0e1d5d62d070f2243706a1d35db999dc2
nellycamille <nellyk_515@hotmail.com>
2015-10-22 17:27:00 -0400

    arreglo espacio, chapter 3

--- homework1.rb
+++ homework1.rb
@@ -5,4 +5,4 @@ puts "Table of Contents".center(line_width)
 puts "-" * line_width
 puts "Chapter 1: Get Started".ljust(line_width_menu) + "Page 1".rjust(line_width_menu)
 puts "Chapter 2: All about cats".ljust(line_width_menu) + "Page 2".rjust(line_width_menu)
-puts "Chapter 3: The parts of a bicycle ".ljust(line_width_menu) + "Page 3".rjust(line_width_menu)
+puts "Chapter 3: The parts of a bicycle".ljust(line_width_menu) + "Page 3".rjust(line_width_menu)
