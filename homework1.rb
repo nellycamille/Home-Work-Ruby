ae9f2670391f4b4140a6e929f5122eed4662bf64
nellycamille <nellyk_515@hotmail.com>
2015-10-22 10:02:24 -0400

    table done

---
+++ homework1.rb
@@ -0,0 +1,8 @@
+line_width = 80
+line_width_menu = 40
+
+puts "Table of Contents".center(line_width)
+puts "-" * line_width
+puts "Chapter 1: Get Started".ljust(line_width_menu) + "Page 1".rjust(line_width_menu)
+puts "Chapter 2: All about cats".ljust(line_width_menu) + "Page 2".rjust(line_width_menu)
+puts "Chapter 3: The parts of a bicycle ".ljust(line_width_menu) + "Page 3".rjust(line_width_menu)
